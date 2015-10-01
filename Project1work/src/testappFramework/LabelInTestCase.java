221645d2d26f18a6bc20687e830eeaeaba3a38e1
Zlati <Zlati@Zlati-PC>
2015-10-01 11:46:45 +0300

    NullPoiner Error

--- Project1work/src/testappFramework/LabelInTestCase.java
+++ Project1work/src/testappFramework/LabelInTestCase.java
@@ -1,18 +1,18 @@
 package testappFramework;
 import java.util.concurrent.TimeUnit;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
-public class LabelInTestCase extends LabelInTestCaseSCRIPT {		
-	private LabelInTestCaseSCRIPT page;
-	@Before
-	public void openBrowser() {
-		page = PageFactory.initElements(driver, LabelInTestCaseSCRIPT.class);
-		page.openB("http://10.75.238.99:3000/cases");
-		driver.get("");
-	}
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+public class LabelInTestCase {		
+	public LabelInTestCaseSCRIPT page;
 	@Test
 	public void createTestCase() {
+	page.openB("http://193.178.152.151:4000/cases");
 	page.clickOnButtonNew();
 	}
 }
--- Project1work/src/testappFramework/LabelInTestCaseSCRIPT.java
+++ Project1work/src/testappFramework/LabelInTestCaseSCRIPT.java
@@ -1,38 +1,46 @@
 package testappFramework;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.thoughtworks.selenium.Wait;
+import com.thoughtworks.selenium.webdriven.commands.WaitForPageToLoad;
 public class LabelInTestCaseSCRIPT {	
-	ChromeOptions options = new ChromeOptions();
-	protected WebDriver driver = new ChromeDriver(options);
-	WebDriverWait wait;
-	 @FindBy(how = How.CSS, using = "button.button.new-test-case-button.blue.right")
-	 private WebElement buttonNew;
-	 public void openB (String url){
-		 driver.get(url);
+	private final WebDriver driver;
+	public LabelInTestCaseSCRIPT(WebDriver driver){
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	public WebDriverWait wait;
+	@FindBy(how = How.CSS, using = "div.button.primary.new-test-case-button.right")
+	 public WebElement buttonNew;
+	 public void openB(String url){
+		driver.get(url);
 	 }
 	    public void clickOnButtonNew () {
-			System.setProperty("webdriver.chrome.driver",
-					"D:\\SELENIUM\\chromedriver_win32\\chromedriver.exe");
-			wait = new WebDriverWait(driver, 4000);
 			wait.until(ExpectedConditions.presenceOfElementLocated(By
-					.cssSelector("button.button.new-test-case-button.blue.right")));
+				.cssSelector("div.button.primary.new-test-case-button.right")));
 			buttonNew.click();
-			wait.until(ExpectedConditions.presenceOfElementLocated(By
-					.cssSelector("select.dropdown.full-width.test-case-type")));
+			//wait.until(ExpectedConditions.presenceOfElementLocated(By
+			//		.cssSelector("select.dropdown.full-width.test-case-type")));
 
 	    }
 }
